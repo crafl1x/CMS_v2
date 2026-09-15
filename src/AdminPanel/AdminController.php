0cd8dd2fbd0855f2a4286c1772fc191f511b6a3f
crafl1x <142533052+crafl1x@users.noreply.github.com>
2026-09-15 17:09:18 +0200

    pass current user data to twig globals

--- src/AdminPanel/AdminController.php
+++ src/AdminPanel/AdminController.php
@@ -4,6 +4,8 @@ namespace App\AdminPanel;
 
 use App\Auth\Auth;
 use App\Controller\Controller;
+use App\Database\Database;
+use PDO;
 
 
 class AdminController extends Controller {
@@ -11,6 +13,17 @@ class AdminController extends Controller {
     public function __construct() {
         parent::__construct();
         Auth::requireLogin('editor');
+
+        $db = Database::connect();
+        $conn = $db->prepare("SELECT `email`,`role` FROM `users` WHERE `id`=:id");
+        $conn->bindValue(":id", Auth::getUserID(), PDO::PARAM_INT);
+        $conn->execute();
+
+        $data = $conn->fetch();
+
+        $this->addToTwigGlobals([
+            "account" => $data
+        ]);
     }
 
 }
